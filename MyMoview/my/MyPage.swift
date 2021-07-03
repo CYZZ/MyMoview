1b487ec902dcdae90b96184ac04b9cd761e93123
chiyuze <18359628931@qq.com>
2021-07-03 17:06:48 +0800

    add my page

---
+++ MyMoview/my/MyPage.swift
@@ -0,0 +1,17 @@
+//
+//  MyPage.swift
+//  MyMoview
+//
+//  Created by CYZ on 2021/7/3.
+//
+
+import Foundation
+
+class MyPage {
+	var name:String
+	var age:Int
+	init(name:String, age:Int) {
+		self.name = name
+		self.age = age
+	}
+}
